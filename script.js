e6481def09ced6188055f97c97723611916f02e7
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2024-11-06 01:16:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const container = document.querySelector(".newOne");
+
+// // let range2 = (start,end) => {
+// //     return Array(end-start+1);
+// // }
+
+// // container.textContent = range2(5,23);
+// // console.log(range2)
+// function newRange(start,end){
+//     return Array(end-start+1).fill(start).map((element,index)=> element+index)
+// }
+// const result = newRange(1,20);
+// container.textContent = "hello",result;
+// console.log("5",newRange(5,15));
